f887d25bab100dbe636bdb4634e66b35e1f498ef
Startarrot <robertchambers4444@gmail.com>
2021-05-01 13:21:47 -0500

    text and stop
    Get text to display and to stop the player while text is up.  pulls up text using X and dismisses it with Z

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,20 +8,29 @@ import flixel.tile.FlxTilemap;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import character.Item;
 import character.Player;
-import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
 import flixel.FlxG;
 import flixel.FlxObject;
+import flixel.text.FlxText;
+import flixel.FlxSprite;
+
+using flixel.util.FlxSpriteUtil;
 
 class PlayState extends FlxState
 {
 	private var player:Player;
 	private var reach:Item;
 
+	
+
 	private var map:FlxOgmo3Loader;
 	private var walls:FlxTilemap;
 	private var ending:Bool;
 	private var won:Bool;
+	var text:FlxText;
+	var background:FlxSprite;
+
+	private var talk:Bool;
 
 	private var levelExit:LevelExit;
 
@@ -29,6 +38,9 @@ class PlayState extends FlxState
 	{
 		super.create();
 
+		talk = false;
+
+		background = new FlxSprite(0,400).makeGraphic(FlxG.width, 100, FlxColor.BLACK);
 
 		//add level
 		map = new FlxOgmo3Loader(AssetPaths.csc303_game_levels__ogmo, AssetPaths.level01__json);
@@ -48,7 +60,7 @@ class PlayState extends FlxState
 		
 		reach = new Item(player);
 		add(reach);
-		
+		text = new FlxText(0, 400, FlxG.width, "This is a gun.  This is probably the murder weapon.  I should check the body", 20);
 	}
 
 	override public function update(elapsed:Float):Void
@@ -59,6 +71,19 @@ class PlayState extends FlxState
 		if (ending)
 			{
 				return;
+			}
+
+		if(talk)
+			{
+				if(FlxG.keys.justPressed.Z)
+					{
+						talk = false;
+						player.canMove = true;
+						text.destroy();
+						background.destroy();
+						background = new FlxSprite(0,400).makeGraphic(FlxG.width, 100, FlxColor.BLACK);
+						text = new FlxText(0, 400, FlxG.width, "This is a gun.  This is probably the murder weapon.  I should check the body", 20);
+					}
 			}
 
 		// if (FlxG.overlap(hero, levelExit))
@@ -73,12 +98,26 @@ class PlayState extends FlxState
 		// FlxG.overlap(hero, keys, pickupKey);
 		// FlxG.collide(hero, walls);
 		// FlxG.collide(hero, doors);
+
+
+		if(FlxG.keys.justPressed.X)
+			talk = gunPickup(text, player, talk, background);
 	}
 
 	private function doneFadeOut()
 	{
 		FlxG.switchState(new states.GameOverState());	
 	}
+
+	public function gunPickup(text, player, talk, back)
+		{
+			add(back);
+			
+			add(text);
+
+			player.canMove = false;
+			return talk = true;
+		}
 
 	// // Opens locked doors if you have a key
 	// private function openDoor(hero:Hero, door:Door)
